427a3d6b27b66d9dd6383da3fb47563966104141
goncalog <goncalogordo@gmail.com>
2020-11-27 14:47:44 +0000

    Replace Seller Mongoose model with Owner model

--- backend/models/owner.js
+++ backend/models/owner.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
 
-const SellerSchema = new Schema({
+const OwnerSchema = new Schema({
     name: { type: String, required: true },
     contact: { type: String, required: true, unique: true },
     rating: { type: Number, required: true, min: 0, max: 5 },
     password: { type: String, required: true, minlength: 8 },
 });
 
-module.exports = mongoose.model('Seller', SellerSchema);
+module.exports = mongoose.model('Owner', OwnerSchema);
