3a15344cf4e0b605e11149b73cfb60499bb1acfc
goncalog <goncalogordo@gmail.com>
2020-11-27 15:58:07 +0000

    Update formatPrice to format prices according to daily prices

--- frontend/src/utils/formatPrice.js
+++ frontend/src/utils/formatPrice.js
@@ -3,7 +3,7 @@ import formatNumber from './formatNumber';
 export default function formatPrice(country, price) {
     switch(country.toLowerCase()) {
         case 'uk':
-            return `£${formatNumber(price)}`;
+            return `£${formatNumber(price)}/day`;
         default:
             return price;
     }
