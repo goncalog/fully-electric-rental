993e8b42ad23fcd930343ce8ed116e60af2cefcd
goncalog <goncalogordo@gmail.com>
2020-11-27 14:45:41 +0000

    Update EV Mongoose model to include rental properties

--- backend/models/ev.js
+++ backend/models/ev.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
 
 const currentDate = new Date();
@@ -8,11 +7,14 @@ const EVSchema = new Schema({
     make: { type: Schema.Types.ObjectId, ref: 'Make', required: true },
     model: { type: Schema.Types.ObjectId, ref: 'Model', required: true },
     year: { type: Number, required: true, min: 1900, max: currentDate.getFullYear() },
-    price: { type: Number, required: true, min: 0 },
+    price_per_day: { type: Number, required: true, min: 0 },
+    deposit: { type: Number, required: true, min: 0 },
+    min_rental_period: { type: String, required: true },
+    included_extras: { type: [String], required: true, default: undefined }, // this is for things like insurance, maintenance, etc. by default this would be an empty array which would affect the way we then create new instances of this model (i.e. it would require using array.push(item))
     mileage: { type: Number, required: true, min: 0 },
     location: { type: Schema.Types.ObjectId, ref: 'Location', required: true },
     image_urls: { type: [String], required: true, default: undefined }, // by default this would be an empty array which would affect the way we then create new instances of this model (i.e. it would require using array.push(item))
-    seller: { type: Schema.Types.ObjectId, ref: 'Seller', required: true },
+    owner: { type: Schema.Types.ObjectId, ref: 'Owner', required: true },
     list_date: { type: Date, default: currentDate, required: true },
     equipment_and_options: { type: [String], required: true, default: undefined }, // by default this would be an empty array which would affect the way we then create new instances of this model (i.e. it would require using array.push(item))
     exterior: {
@@ -25,6 +27,7 @@ const EVSchema = new Schema({
     },
     vehicle_identification_number: { type: String, minlength: 17, maxlength: 17, required: true },
     full_vehicle_inspection: { type: Boolean, required: true },
+    pco_license: { type: Boolean, required: true },
 });
 
 // Virtual for ev's url
