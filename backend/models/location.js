4734e829f6a84eb057671660f3db7e5ab2d27b1a
goncalog <goncalogordo@gmail.com>
2020-11-27 14:46:44 +0000

    Update Location Mongoose model to include rental properties

--- backend/models/location.js
+++ backend/models/location.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
 
 const LocationSchema = new Schema({
+    name: { type: String, required: true },
     city: { type: String, required: true },
     country: { type: String, required: true },
 });
