764cf312e818e6ff46d63c9eada3df586328c64a
goncalog <goncalogordo@gmail.com>
2020-11-27 15:57:52 +0000

    Update formatPrice tests to format prices according to daily prices

--- frontend/src/__tests__/utils/formatPrice.test.js
+++ frontend/src/__tests__/utils/formatPrice.test.js
@@ -1,15 +1,8 @@
 import formatPrice from '../../utils/formatPrice';
 
 describe('formatPrice', () => {
-    test('correctly formats prices below 1000', () => {
-        expect(formatPrice('uk', '900')).toBe('£900');
-    });
-
-    test('correctly formats prices equal or larger than 1000', () => {
-        expect(formatPrice('uk', '1000')).toBe('£1,000');
-        expect(formatPrice('uk', '10000')).toBe('£10,000');
-        expect(formatPrice('uk', '50000')).toBe('£50,000');
-        expect(formatPrice('uk', '100000')).toBe('£100,000');
-        expect(formatPrice('uk', '500000')).toBe('£500,000');
+    test('correctly formats prices', () => {
+        expect(formatPrice('uk', '9.5')).toBe('£9.5/day');
+        expect(formatPrice('uk', '90')).toBe('£90/day');
     });
 });
