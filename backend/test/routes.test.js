161d9a5e15cb07a221cb3d542bae661538a9b3a3
goncalog <goncalogordo@gmail.com>
2020-12-01 16:35:35 +0000

    Fix bug in sign up route tests

--- backend/test/routes.test.js
+++ backend/test/routes.test.js
@@ -271,7 +271,7 @@ describe('Routes testing', function () {
             .type('form')
             .send({ name: 'Miss Zoe', contact: 'zoe@gmail.com', password: '12345678' })
             .expect(function(res) {
-                res.body.userId = '555666777';
+                if (res.body.userId) res.body.userId = '555666777';
               })
             .expect('Content-type', /json/)
             .expect({ title: 'Miss Zoe signed up', userId: '555666777' })
